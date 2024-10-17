45e29d7a81376236e614b787ee7524a926075b29
Gilberts Ahumada <gilbertsahumada@gmail.com>
2024-10-17 16:20:40 -0300

    Update CCIP_Sender.sol

--- CCIP_Sender.sol
+++ CCIP_Sender.sol
@@ -1,1 +1,38 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {IERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+
+contract Messenger {
+    IERC20 private s_linkToken;
+    IRouterClient private s_router;
+
+    constructor(address _router, address _link) {
+        s_linkToken = IERC20(_link);
+        s_router = IRouterClient(_router);
+    }
+
+    function send(
+        uint64 _destinationChainSelector,
+        address _receiver,
+        string calldata _text
+    ) external {
+        Client.EVM2AnyMessage memory evm2AnyMessage = Client.EVM2AnyMessage({
+            receiver: abi.encode(_receiver),
+            data: abi.encode(_text),
+            tokenAmounts: new Client.EVMTokenAmount[](0),
+            extraArgs: "",
+            feeToken: address(s_linkToken)
+        });
+
+        s_linkToken.approve(address(s_router), 1e18 ether);
+        s_router.ccipSend(_destinationChainSelector, evm2AnyMessage);
+    }
+}
+
+
+//0x9c9f3052Fcdde50d6E1f9241d6b3b35044FACec0
+//0xd6b8f5Ddf0dA19C4bEF691A93666605A451A39Cc
